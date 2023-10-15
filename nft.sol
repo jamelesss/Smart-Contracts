35a3da2f0e38f1366d737eaba0b5458fb99fb193
Sanjana Tikone <94596402+jamelesss@users.noreply.github.com>
2023-10-15 11:56:55 +0530

    Add files via upload

---
+++ nft.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable(msg.sender) {
+    struct certificate {
+        string name;
+        string content;
+    }
+
+    mapping(uint256 => certificate) public certificates;
+
+    constructor() ERC721("CertificateNFT", "certNFT") {
+        // Set the name and symbol for your NFT.
+    }
+
+    function mintCertificate(address recipient, uint256 tokenId, string memory name, string memory content) public onlyOwner {
+        _mint(recipient, tokenId);
+        certificates[tokenId] = certificate(name, content);
+    }
+}
